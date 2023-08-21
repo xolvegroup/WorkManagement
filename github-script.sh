46951996b74443aab434464ae5a017bf371b3da6
xolvegirindra <107811649+xolvegirindra@users.noreply.github.com>
2023-08-21 20:29:16 +0530

    Update github-script.sh

--- github-script.sh
+++ github-script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #set -x
 
-#rm -rf moquiprod
+rm -rf moqui
 echo "download from repository"
-git clone https://github.com/moqui/moqui-framework.git moquiprod
-cd moquiprod
+git clone https://github.com/moqui/moqui-framework.git moqui && cd moqui
 ./gradlew getComponent -Pcomponent=HiveMind
 ./gradlew downloadOpenSearch
 cd runtime/component
